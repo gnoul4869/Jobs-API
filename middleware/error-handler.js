a446fe95c383381263fbdd9c73342824723508cc
gnoul_ <ttluong4869@gmail.com>
2021-10-11 20:00:35 +0700

    Add new custom error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,26 @@
-const { CustomAPIError } = require('../errors');
+// const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-    if (err instanceof CustomAPIError) {
-        return res.status(err.statusCode).json({ msg: err.message });
+    let customError = {
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'Something went wrong, please try again later',
+    };
+
+    // if (err instanceof CustomAPIError) {
+    //     return res.status(err.statusCode).json({ msg: err.message });
+    // }
+
+    if (err.code && err.code === 11000) {
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+        // customError.msg = 'This email has already been registered';
+        customError.msg = `Duplicate value for ${Object.keys(
+            err.keyValue
+        )} field`;
     }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+    return res.status(customError.statusCode).json({ msg: customError.msg });
+    // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
 };
 
 module.exports = errorHandlerMiddleware;
