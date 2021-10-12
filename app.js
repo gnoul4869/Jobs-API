a5a52dd48064148cff1e899d3c3a2125c275d766
gnoul_ <ttluong4869@gmail.com>
2021-10-12 14:24:46 +0700

    Setup security packages

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 const connectDB = require('./db/connect');
@@ -13,7 +20,18 @@ const authenticateUser = require('./middleware/authentication');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+    rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+    })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 // extra packages
 
 // routes
